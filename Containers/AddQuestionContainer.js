fa5fa5e74382e975af8f705beca5d1e1f5b99d5b
bwagner <bradford.wagner@blackrock.com>
2018-01-05 17:40:45 -0500

    Code cleanup

--- Containers/AddQuestionContainer.js
+++ Containers/AddQuestionContainer.js
@@ -1,67 +1,67 @@
 // @flow
-import type { NavigationProps } from '../Lib/Navigation'
-import type { Action } from '../Components/ActionBar';
+import type {NavigationProps} from '../Lib/Navigation'
+import type {Action} from '../Components/ActionBar';
+import {ActionBar} from '../Components/ActionBar';
 
-import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
-import { Deck, Question } from '../Lib/Deck';
-import { TextBox } from '../Components/TextBox';
-import { Layout } from '../Util/CommonStyles';
-import { ActionBar } from '../Components/ActionBar';
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {Deck, Question} from '../Lib/Deck';
+import {TextBox} from '../Components/TextBox';
+import {Layout} from '../Util/CommonStyles';
 
 export type AddQuestionContainerParams = {
-    deck: Deck,
-    onAdd: Function
+  deck: Deck,
+  onAdd: Function
 }
 
 type Props = {
-    navigation: NavigationProps<AddQuestionContainerParams>
+  navigation: NavigationProps<AddQuestionContainerParams>
 }
 type State = {
-    question: string,
-    answer: string
+  question: string,
+  answer: string
 }
 
 export class AddQuestionContainer extends Component<Props, State> {
-    state = {
-        question: "",
-        answer: ""
-    }
+  state = {
+    question: "",
+    answer: ""
+  };
 
-    addQuestion = () => {
-        const q = new Question()
-        q.question = this.state.question
-        q.answer = this.state.answer
-        const deck = this.props.navigation.state.params.deck
-        deck.questions.push(q)
-        this.props.navigation.goBack()
-        this.props.navigation.state.params.onAdd()
-    }
+  addQuestion = () => {
+    const q = new Question();
+    q.question = this.state.question;
+    q.answer = this.state.answer;
+    const deck = this.props.navigation.state.params.deck;
+    deck.questions.push(q);
+    this.props.navigation.goBack();
+    this.props.navigation.state.params.onAdd()
+  };
 
-    buildActions = () => {
-        const addQuestion: Action = {
-            title: "Add Question",
-            onPress: () => this.addQuestion()
-        }
-        return [addQuestion]
-    }
+  buildActions = () => {
+    const addQuestion: Action = {
+      title: "Add Question",
+      onPress: () => this.addQuestion()
+    };
+    return [addQuestion]
+  };
 
-    render = () => (
-        <View style={Layout.Colum, Layout.Flex}>
-            <View style={[Layout.Flex]}>
-                <TextBox
-                    value={this.state.question}
-                    onChange={(question) => this.setState({ question })}
-                    placeholder="Question"
-                />
-                <TextBox
-                    value={this.state.answer}
-                    onChange={(answer) => this.setState({ answer })}
-                    placeholder="Answer"
-                />
-            </View>
+  render = () => (
+    <View style={Layout.Colum, Layout.Flex}>
+      <View style={[Layout.Flex]}>
+        <TextBox
+          value={this.state.question}
+          onChange={(question) => this.setState({question})}
+          placeholder="Question"
+        />
+        <TextBox
+          value={this.state.answer}
+          onChange={(answer) => this.setState({answer})}
+          placeholder="Answer"
+        />
+      </View>
 
-            <ActionBar actions={this.buildActions()} />
-        </View>
-    )
+      <ActionBar actions={this.buildActions()}/>
+    </View>
+  )
 }
